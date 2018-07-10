5d85d4336342aaca45bc5b6a700afce44d1e75b2
giriash <linzhe940604@gmail.com>
2018-07-10 14:57:02 -0700

    section 2

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,13 +11,18 @@
 
       render() {
           return (
-              <div>
+              <div className="search-bar">
                   <input
                       value = {this.state.value}
-                      onChange={event => this.setState({term: event.target.value})}/>
+                      onChange={event => this.onInputChange(event.target.value)}/>
               </div>
 
           );
+      }
+
+      onInputChange(term) {
+         this.setState({term});
+         this.props.onSearchTermChange(term);
       }
 
   }
